5ec1385ef8e748ff11ea5c98a4934084e17cefed
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-08-25 14:01:55 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog 
+  
+  def initialize(dogName, dogBreed)
+    @dogName = name
+    @dogBreed = breed
+  end
+  
+  def name=(name)
+    @dogName = name
+  end
+  
+  def name
+    dogName
+  end
+  
+  def breed=(breed)
+    @dogBreed = breed
+  end
+  
+  def breed
+    dogBreed
+  end
+end
+
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+
+class Person
+  def name=(name)
+    @personName = name
+  end
+  
+  def name
+    personName
+  end
+  
+  def job=(job)
+    @personJob = job
+  end
+  
+  def job
+    personJob
+  end
+end
